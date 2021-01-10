4eeb0cda533bb2d222902d37b1490989653b1501
RyukieSama <ryukie.sama@gmail.com>
2021-01-10 13:21:43 +0800

    ADD: <剑指 Offer 06. 从尾到头打印链表>

---
+++ LeetCode.playground/Pages/剑指 Offer 06. 从尾到头打印链表.xcplaygroundpage/Contents.swift
@@ -0,0 +1,51 @@
+//: [Previous](@previous)
+
+/*:
+ 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+ 
+ 
+ 示例 1：
+ 
+ 输入：head = [1,3,2]
+ 输出：[2,3,1]
+ */
+
+import Foundation
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+let l1 = ListNode(1)
+let l2 = ListNode(3)
+let l3 = ListNode(2)
+
+l1.next = l2
+
+l2.next = l3
+
+l3.next = nil
+
+func reversePrint(_ head: ListNode?) -> [Int] {
+    guard let head = head else { return [] }
+    var result: [Int] = []
+    var currentNode: ListNode? = head
+    
+    while currentNode != nil {
+        if let nd = currentNode {
+            result.insert(nd.val, at: 0)
+            currentNode = nd.next
+        }
+    }
+    
+    return result
+}
+
+reversePrint(l1)
+
+//: [Next](@next)
