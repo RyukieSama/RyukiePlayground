6d82db700dbdb9855152026177495d877e655dc9
RyukieSama <ryukie.sama@gmail.com>
2021-04-15 23:01:27 +0800

    MOD: <剑指 Offer 11. 旋转数组的最小数字>

--- LeetCode.playground/Pages/剑指 Offer 11. 旋转数组的最小数字.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/剑指 Offer 11. 旋转数组的最小数字.xcplaygroundpage/Contents.swift
@@ -22,10 +22,45 @@ import Foundation
  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
-let num = [3,4,5,1,2]
+let num = [1, 1]
 
 func minArray(_ numbers: [Int]) -> Int {
+    // 正序 发现后一个比前一个小，就是后一个
+    // 逆序 发现下一个比上一个大，就是上一个
     
+    if numbers.count == 1 {
+        return numbers[0]
+    }
+    
+    if numbers.count == 2 {
+        return numbers[0] >= numbers[1] ? numbers[1] : numbers[0]
+    }
+    
+    var offset = 0
+    let halfOffset = Int(floor(Double(numbers.count) / 2.0))
+    
+    if numbers[0] < numbers[numbers.count - 1] {
+        return numbers[0]
+    }
+    
+    while offset <= halfOffset {
+        
+        // 正序的
+        if numbers[offset] > numbers[offset + 1] {
+            return numbers[offset + 1]
+        }
+        
+        // 逆序的
+        if numbers[numbers.count - 1 - offset] < numbers[numbers.count - 2 - offset] {
+            return numbers[numbers.count - 1 - offset]
+        }
+        
+        offset += 1
+    }
+    
+    return numbers[0]// 全部相同随便取个
 }
+
+minArray(num)
 
 //: [Next](@next)
