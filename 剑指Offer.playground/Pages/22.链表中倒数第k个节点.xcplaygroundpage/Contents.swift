bf9e0a21ed284739f31aa4c420844b62a95acbc9
RyukieSama <ryukie.sama@gmail.com>
2021-07-14 12:12:52 +0800

    ADD: <22.链表中倒数第k个节点>
    ADD: <24.反转链表>

---
+++ 剑指Offer.playground/Pages/22.链表中倒数第k个节点.xcplaygroundpage/Contents.swift
@@ -0,0 +1,68 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ 
+ 输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+
+ 例如，一个链表有 6 个节点，从头节点开始，它们的值依次是 1、2、3、4、5、6。这个链表的倒数第 3 个节点是值为 4 的节点。
+
+ 示例：
+
+```C++
+ 给定一个链表: 1->2->3->4->5, 和 k = 2.
+
+ 返回链表 4->5.
+ 通过次数193,790提交次数246,166
+```
+ 
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ 
+ */
+
+/**
+ 方案一：
+ 栈
+ */
+func getKthFromEnd(_ head: ListNode?, _ k: Int) -> ListNode? {
+    var node = head
+    
+    var temp: [ListNode] = []
+    
+    while let currentNode = node {
+        temp.insert(currentNode, at: 0)
+        if temp.count > k {
+            temp.removeLast()
+        }
+        node = currentNode.next
+    }
+    
+    return temp.last
+}
+
+/**
+ 方案二：
+ 差速双指针
+ */
+func getKthFromEnd2(_ head: ListNode?, _ k: Int) -> ListNode? {
+    var right: ListNode? = head, left: ListNode? = head
+    
+    // 移动到 第 k 个节点
+    for _ in 0..<k-1 {
+        right = right?.next
+    }
+    
+    while let _ = right?.next {
+        right = right?.next
+        left = left?.next
+    }
+    
+    return left
+}
+
+
+
+//: [Next](@next)
---
+++ 剑指Offer.playground/Pages/24.反转链表.xcplaygroundpage/Contents.swift
@@ -0,0 +1,56 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+
+  
+
+ 示例:
+
+```C++
+ 输入: 1->2->3->4->5->NULL
+ 输出: 5->4->3->2->1->NULL
+```
+
+ 限制：
+
+```C++
+ 0 <= 节点个数 <= 5000
+```
+ 
+ 注意：本题与主站 206 题相同：https://leetcode-cn.com/problems/reverse-linked-list/
+
+* 来源：力扣（LeetCode）
+* 链接：https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof
+* 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+func reverseList(_ head: ListNode?) -> ListNode? {
+    var node: ListNode?
+    var temp: ListNode? = head
+    
+    while let current = temp {
+        let n = ListNode(current.val)
+        n.next = node
+        node = n
+        temp = current.next
+    }
+    
+    return node
+}
+
+let l1 = ListNode(1)
+let l2 = ListNode(3)
+let l3 = ListNode(2)
+
+l1.next = l2
+
+l2.next = l3
+
+l3.next = nil
+
+reverseList(l1)
+
+//: [Next](@next)
