3bcc92af0c86d0c6b84c4e56c709c450a35b95f0
RyukieSama <ryukie.sama@gmail.com>
2022-03-18 20:53:15 +0800

    ADD: <52.两个链表的第一个公共节点>

--- 剑指Offer.playground/Sources/DataStruct/ListNode.swift
+++ 剑指Offer.playground/Sources/DataStruct/ListNode.swift
@@ -1,6 +1,11 @@
 import Foundation
 
-public class ListNode {
+public class ListNode: Equatable {
+    public static func == (lhs: ListNode, rhs: ListNode) -> Bool {
+//        guard let lObj = lhs as? AnyObject, let rObj = rhs as AnyObject
+        Unmanaged.passUnretained(lhs).toOpaque() == Unmanaged.passUnretained(rhs).toOpaque()
+    }
+    
     public var val: Int
     public var next: ListNode?
     public init(_ val: Int) {
