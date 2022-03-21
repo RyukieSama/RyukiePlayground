f4f0e22627a9f7ab63a7526f1fffb63e764dcd30
RyukieSama <ryukie.sama@gmail.com>
2022-03-21 12:11:07 +0800

    DEL: <Useless>

--- 剑指Offer.playground/Sources/DataStruct/ListNode.swift
+++ 剑指Offer.playground/Sources/DataStruct/ListNode.swift
@@ -2,7 +2,6 @@ import Foundation
 
 public class ListNode: Equatable {
     public static func == (lhs: ListNode, rhs: ListNode) -> Bool {
-//        guard let lObj = lhs as? AnyObject, let rObj = rhs as AnyObject
         Unmanaged.passUnretained(lhs).toOpaque() == Unmanaged.passUnretained(rhs).toOpaque()
     }
     
