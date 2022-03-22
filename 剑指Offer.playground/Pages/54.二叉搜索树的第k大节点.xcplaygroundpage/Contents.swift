da9ee06f6dfe407fb275d5cb81df8173758c6327
RyukieSama <ryukie.sama@gmail.com>
2022-03-22 16:24:53 +0800

    ADD: <54.二叉搜索树的第k大节点>

---
+++ 剑指Offer.playground/Pages/54.二叉搜索树的第k大节点.xcplaygroundpage/Contents.swift
@@ -0,0 +1,81 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ 给定一棵二叉搜索树，请找出其中第 k 大的节点的值。
+ 
+ 示例 1:
+ 
+ 输入: root = [3,1,4,null,2], k = 1
+ 
+ ```
+ 3
+ / \
+ 1   4
+ \
+    2
+ ```
+ 
+ 输出: 4
+ 
+ 示例 2:
+ 
+ 输入: root = [5,3,6,2,4,null,null,1], k = 3
+ 
+ ```
+ 5
+ / \
+ 3   6
+ / \
+ 2   4
+ /
+ 1
+ ```
+ 
+ 输出: 4
+  
+ 限制：
+ 
+ 1 ≤ k ≤ 二叉搜索树元素个数
+ 
+ 来源：力扣（LeetCode）
+ 
+ 链接：https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof
+ 
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+func kthLargest(_ root: TreeNode?, _ k: Int) -> Int {
+    var inorderArr: [Int] = []
+    
+    func inorder(_ node: TreeNode?) {
+        guard let n = node else {
+            return
+        }
+        
+        inorder(n.right)
+        inorderArr.append(n.val)
+        inorder(n.left)
+    }
+    
+    inorder(root)
+    
+    return inorderArr[k - 1]
+}
+
+let node = TreeNode(5,
+                    TreeNode(3,
+                             TreeNode(2,
+                                      TreeNode(1),
+                                      nil),
+                             TreeNode(4,
+                                      nil,
+                                      nil)),
+                    TreeNode(6,
+                             nil,
+                             nil))
+
+kthLargest(node, 3)
+
+//: [Next](@next)
