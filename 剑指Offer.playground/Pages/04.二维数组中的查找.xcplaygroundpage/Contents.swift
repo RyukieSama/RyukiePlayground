00f83cc231f137aea1c6eeff38b03cc1839c9be3
RyukieSama <ryukie.sama@gmail.com>
2022-04-27 19:19:59 +0800

    MOD: <04.二维数组中的查找>

--- 剑指Offer.playground/Pages/04.二维数组中的查找.xcplaygroundpage/Contents.swift
+++ 剑指Offer.playground/Pages/04.二维数组中的查找.xcplaygroundpage/Contents.swift
@@ -2,12 +2,11 @@
 /*:
  在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个高效的函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
  
-  
- 
  示例:
  
  现有矩阵 matrix 如下：
  
+ ```
  [
  [1,   4,  7, 11, 15],
  [2,   5,  8, 12, 19],
@@ -15,11 +14,11 @@
  [10, 13, 14, 17, 24],
  [18, 21, 23, 26, 30]
  ]
+ ```
+ 
  给定 target = 5，返回 true。
  
  给定 target = 20，返回 false。
- 
-  
  
  限制：
  
@@ -27,19 +26,90 @@
  
  0 <= m <= 1000
  
-  
- 
  注意：本题与主站 240 题相同：https://leetcode-cn.com/problems/search-a-2d-matrix-ii/
- 
- 
- 
- 
+
  链接：https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof
- 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
 import UIKit
+
+var array =  [
+    [1,   4,  7, 11, 15],
+    [2,   5,  8, 12, 19],
+    [3,   6,  9, 16, 22],
+    [10, 13, 14, 17, 24],
+    [18, 21, 23, 26, 30]
+]
+
+let target = 5
+
+// 2022-04-27 从右上角开始
+func findNumberIn2DArrayA(_ matrix: [[Int]], _ target: Int) -> Bool {
+    guard matrix.isEmpty == false, let first = matrix.first, first.isEmpty == false else {
+        return false
+    }
+    var x = first.count - 1, y = 0
+    
+    while y < matrix.count {
+        let line = matrix[y]
+        let lineFinal = line[line.count - 1]
+        
+        if lineFinal >= target {
+            while x >= 0 {
+                if line[x] != target {
+                    x -= 1
+                }
+                else {
+                    return true
+                }
+            }
+        }
+        
+        y += 1
+        x = first.count - 1
+    }
+    
+    return false
+}
+
+func findNumberIn2DArrayAA(_ matrix: [[Int]], _ target: Int) -> Bool {
+    guard matrix.isEmpty == false, let first = matrix.first, first.isEmpty == false else {
+        return false
+    }
+    var x = first.count - 1, y = 0
+    
+    while y < matrix.count, x >= 0 {
+        let value = matrix[y][x]
+        if value > target { x -= 1 }
+        else if value < target { y += 1 }
+        else { return true }
+    }
+    
+    return false
+}
+
+// 左下角开始
+func findNumberIn2DArrayB(_ matrix: [[Int]], _ target: Int) -> Bool {
+    guard matrix.isEmpty == false, let first = matrix.first, first.isEmpty == false else {
+        return false
+    }
+    var x = 0, y = matrix.count - 1
+    
+    while x < first.count, y >= 0 {
+        let value = matrix[y][x]
+        if value > target { y -= 1 }
+        else if value < target { x += 1 }
+        else { return true }
+    }
+    
+    return false
+}
+
+findNumberIn2DArrayA(array, target)
+findNumberIn2DArrayAA(array, target)
+findNumberIn2DArrayB(array, target)
 
 // MARK: - 暴力遍历 O(nm) O(1)
 func hard(_ matrix: [[Int]], _ target: Int) -> Bool {
@@ -108,15 +178,6 @@ func ruleWhile(_ matrix: [[Int]], _ target: Int) -> Bool {
     return false
 }
 // MARK: - Test
-var array =  [
-    [1,   4,  7, 11, 15],
-    [2,   5,  8, 12, 19],
-    [3,   6,  9, 16, 22],
-    [10, 13, 14, 17, 24],
-    [18, 21, 23, 26, 30]
-]
-
-let target = 23
 
 func findNumberIn2DArray(_ matrix: [[Int]], _ target: Int) -> Bool {
     //    return hard(array, target)
