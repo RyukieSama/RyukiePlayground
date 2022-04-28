a2211729c5705c2f68ffd90669914e070338453b
RyukieSama <ryukie.sama@gmail.com>
2022-04-28 15:38:14 +0800

    MOD: <05.替换空格>

--- 剑指Offer.playground/Pages/05.替换空格.xcplaygroundpage/Contents.swift
+++ 剑指Offer.playground/Pages/05.替换空格.xcplaygroundpage/Contents.swift
@@ -3,20 +3,18 @@
 /*:
  请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
 
-  
-
  示例 1：
 
  输入：s = "We are happy."
+ 
  输出："We%20are%20happy."
-  
 
  限制：
 
  0 <= s 的长度 <= 10000
-
  
  链接：https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof
+ 
  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
@@ -24,9 +22,7 @@ import Foundation
 
 let str = "We are happy."
 
-func replaceSpace(_ s: String) -> String {
-//    return s.replacingOccurrences(of: " ", with: "%20")
-    
+func replaceSpace(_ s: String) -> String {    
     // MARK: - 不使用系统API
     var temp = ""
     for c in s {
