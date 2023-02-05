4eca04e8faf116d58dedd70cb3d36b231ef26db6
RyukieSama <ryukie.sama@gmail.com>
2023-02-05 21:08:34 +0800

    MOD: <打印从 1 到最大的 n 位数>

--- 剑指Offer.playground/Pages/17. 打印从1到最大的n位数.xcplaygroundpage/Contents.swift
+++ 剑指Offer.playground/Pages/17. 打印从1到最大的n位数.xcplaygroundpage/Contents.swift
@@ -14,12 +14,68 @@
  用返回一个整数列表来代替打印
  n 为正整数
  
- 
  链接：https://leetcode-cn.com/problems/da-yin-cong-1dao-zui-da-de-nwei-shu-lcof
  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
 import Foundation
+
+/**
+ 2023-02-05
+ 
+ 虽慢满足题意，并且能过用例，但是不能大数的处理是个坑
+ */
+func printNumbers1(_ n: Int) -> [Int] {
+    var res: [Int] = []
+    var temp = NSDecimalNumber(decimal: pow(10, n)).intValue
+    for val in 1..<temp {
+        res.append(val)
+    }
+    return res
+}
+
+//printNumbers1(2)
+
+/**
+ 2023-02-05
+ 用字符串处理数
+ */
+func printNumbers2(_ n: Int) {
+    if n < 1 { return }
+    var arr: [Int] = Array(repeating: 0, count: n)
+    var flag = true
+    
+    while flag {
+        if (arr[n - 1] == 9) { // 个位为9 需要进位了
+            arr[n - 1] = 0
+            var leftIdx = n - 2
+            
+            while leftIdx >= 0, flag {
+                if arr[leftIdx] == 9 { // 上一位是 9 也需要进位
+                    if leftIdx == 0 { // 最高位需要进位，也就是满了，跳出循环
+                        flag = false
+                    }
+                    else {
+                        arr[leftIdx] = 0
+                        leftIdx -= 1 // 继续进位
+                    }
+                }
+                else {
+                    arr[leftIdx] += 1
+                    print(arr)// 完成这一次 +1
+                    break // 跳出内循环
+                }
+            }
+        }
+        else {// 不需要进位
+            arr[n - 1] += 1
+            print(arr) // 完成一次 +1
+        }
+    }
+}
+
+printNumbers2(2)
+printNumbers2(3)
 
 func printNumbers(_ n: Int) -> [Int] {
     // 根据位数确定最大数
@@ -31,5 +87,5 @@ func printNumbers(_ n: Int) -> [Int] {
     return result
 }
 
-printNumbers(2)
+//printNumbers(2)
 //: [Next](@next)
